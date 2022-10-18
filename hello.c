34e5f0654c5a71090e9bc7151a9742c77d0dda22
AlbertoDelRosarioJr <78116227+AlbertoDelRosarioJr@users.noreply.github.com>
2022-10-18 10:23:44 -0600

    hello world

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main() {
+	printf("Hello World\n");
+}
+
+// in terminal to compile run command:		gcc -o hello hello.c
+
+// "hello" can be changed to whatever you
+// want to name the executable file
+
+// you should now have a executable file in your folder
+
+// in terminal to run the executable use command: 		./hello
+
+// should output the result of your program
